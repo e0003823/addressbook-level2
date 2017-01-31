24fbb2d13385bce95800659a06a13592ded7df7b
Shawn Lin Jingjue <shawnljj@gmail.com>
2017-01-31 14:10:16 +0800

    Generated getters for “feedbackToUser”, code auto refactored calls to the member, to call the getter.

--- src/seedu/addressbook/commands/CommandResult.java
+++ src/seedu/addressbook/commands/CommandResult.java
@@ -11,9 +11,9 @@ import java.util.Optional;
 public class CommandResult {
 
     /** The feedback message to be shown to the user. Contains a description of the execution result */
-    public final String feedbackToUser;
+    private final String feedbackToUser;
 
-    /** The list of persons that was produced by the command */
+	/** The list of persons that was produced by the command */
     private final List<? extends ReadOnlyPerson> relevantPersons;
 
     public CommandResult(String feedbackToUser) {
@@ -33,4 +33,10 @@ public class CommandResult {
         return Optional.ofNullable(relevantPersons);
     }
 
+	/**
+	 * @return the feedbackToUser
+	 */
+	public String getFeedbackToUser() {
+		return feedbackToUser;
+	}
 }
